3b81b848dab8fa23eee2a2983532a3a26d5a37be
Robot <notarobot7811@gmail.com>
2025-02-15 15:02:47 -0600

    Todays Work
    Turn modal into a class, even though it was working fine without it, but it looks a little nicer.  Got all the buttons working and it loops through if it gets to the last person.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,14 @@
 const numberOfUsers = 12;
 const galleryArea = document.getElementById('gallery');
-let rawData = {};
+let userData = {};
 let card = '';
+console.log(userData.length)
+//----------------------// 
+//-- Gallery Of Users --//
+//----------------------// 
+
+
+//Show Initial Users
 displayUsers()
 
 //displayUsers(): Main fucution for gathering, processing and posting users
@@ -14,17 +21,18 @@ async function displayUsers() {
     }
     // console.log(finalHTML)
     galleryArea.insertAdjacentHTML('beforeend', finalHTML)
-    card = document.querySelector('.card')
+    card = document.querySelectorAll('.card');
 
 }
 
+//fetchData() reach out to the API to generate users
 async function fetchData() {
     try {
-        const response = await fetch(`https://randomuser.me/api/?inc=name,picture,email,location&results=${numberOfUsers}`);
+        const response = await fetch(`https://randomuser.me/api/?inc=name,picture,email,location,dob,phone,id&results=${numberOfUsers}`);
         const data = await response.json();
-        // console.log(data);
-        rawData = data.results;
+        userData = data.results;
         return data.results;
+
     } catch (error) {
         console.error('Error:', error);
     }
@@ -49,35 +57,108 @@ function generateUserHTML(data,id) {
     return userHTML
 };
 
+//----------------------// 
+//-- Event Listeners --//
+//----------------------// 
 
-
-
-
-
+//Event Listener: Watching for card clicks to initate popup
 galleryArea.addEventListener('click', (e) => {
-    {console.log(e.target.closest('.card'))}
-    
+    const activeCard = e.target.closest('.card')  //get the card
+    if(!activeCard) return //if card not found, do nothing.
+    const activeCardIndex = activeCard.dataset.id //get the card id
+    createModal(activeCardIndex)
 })
 
+function createModal(cardIndex){
+    const activeCardData = userData[cardIndex] //get the corresponding dataset
+    const activeModal = new Modal(activeCardData, cardIndex) //pass it to the Modal class
+    activeModal.launchModal()
+}
 
-{/* <div class="modal-container">
-                <div class="modal">
-                    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-                    <div class="modal-info-container">
-                        <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-                        <h3 id="name" class="modal-name cap">name</h3>
-                        <p class="modal-text">email</p>
-                        <p class="modal-text cap">city</p>
-                        <hr>
-                        <p class="modal-text">(555) 555-5555</p>
-                        <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-                        <p class="modal-text">Birthday: 10/21/2015</p>
-                    </div>
+//----------------------// 
+//-- Modal Popup Code --//
+//----------------------// 
+
+class Modal {
+    constructor(data, index){
+        this.data = data;
+        this.index = index;
+        this.html = ''
+        this.modalContainer = ''
+        this.modalClose = ''
+        this.modalNext = ''
+        this.modalPrevious = ''
+    }
+
+    launchModal(){
+        this.buildModalHTML(this.data);
+        galleryArea.insertAdjacentHTML('afterend', this.html);
+        this.modalContainer = document.querySelector('.modal-container');
+        this.modalClose  = document.querySelector('#modal-close-btn');
+        this.modalNext  = document.querySelector('#modal-next');
+        this.modalPrevious  = document.querySelector('#modal-prev');
+        this.initiateEventListers();
+    }
+
+    initiateEventListers(){
+        this.modalClose.addEventListener('click', (e) => {
+            this.modalContainer.remove()
+
+        })
+
+        this.modalNext.addEventListener('click', (e) => {
+            this.modalContainer.remove()
+            let newIndex = (this.index + 1)
+            if(this.index === (numberOfUsers - 1)) {
+                newIndex = 0
+            }
+            console.log(newIndex)
+            console.log(typeof newIndex)
+            createModal(newIndex)
+        });
+
+        this.modalPrevious.addEventListener('click', (e) => {
+            this.modalContainer.remove()
+            let newIndex = (this.index - 1)
+            if(this.index === 0) {
+                newIndex = (numberOfUsers - 1)
+            }
+
+            console.log(newIndex)
+            console.log(typeof newIndex)
+            createModal(newIndex)
+        });
+    }
+
+    buildModalHTML(){
+        console.log(this.data)
+        this.html = 
+        `<div class="modal-container">
+            <div class="modal">
+                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                <div class="modal-info-container">
+                    <img class="modal-img" src="${this.data.picture.large}" alt="profile picture of ${this.data.name.first} ${this.data.name.last}">
+                    <h3 id="name" class="modal-name cap">${this.data.name.first} ${this.data.name.last}</h3>
+                    <p class="modal-text">${this.data.email}</p>
+                    <p class="modal-text cap">${this.data.location.city}</p>
+                    <hr>
+                    <p class="modal-text">${this.data.phone}</p>
+                    <p class="modal-text">${this.data.location.street.number} ${this.data.location.street.name}, ${this.data.location.city}, ${this.data.location.state} ${this.data.location.postcode}</p>
+                    <p class="modal-text">Birthday: ${this.convertDate()}</p>
                 </div>
-
-                // IMPORTANT: Below is only for exceeds tasks 
+            </div>
+        
                 <div class="modal-btn-container">
                     <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
                     <button type="button" id="modal-next" class="modal-next btn">Next</button>
                 </div>
-            </div> */}
+            </div>`;
+        }
+        
+        
+    convertDate(){
+        const date = new Date(this.data.dob.date);
+        const formattedDate = date.toLocaleDateString('en-US'); 
+        return formattedDate
+    };
+}
